b6eea468555199e502f9800c976fdf0929fe1e8f
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-31 16:42:10 -0500

    donations migration authored

--- data/migrations/20200131160044_donations.js
+++ data/migrations/20200131160044_donations.js
@@ -1,3 +1,32 @@
-exports.up = tbl => {};
+const knexfile = require('../../knexfile');
+const knex = require('knex')(knexfile.development);
 
-exports.down = tbl => {};
+exports.up = tbl => {
+	return tbl.schema.createTable('donations', tbl => {
+		tbl.increments();
+
+		tbl.integer('donated_by')
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('users')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+
+		tbl.integer('donated_for')
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('campaigns')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+
+		tbl.timestamp('donated_at', { useTz: true })
+			.notNullable()
+			.defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = tbl => {
+	return tbl.schema.dropTableIfExists('donations');
+};
