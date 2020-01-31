121b4d6ef5fd1960352d37a1c4652673cacf5e8c
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-31 17:01:32 -0500

    donations seed data authored

--- data/seeds/004donations.js
+++ data/seeds/004donations.js
@@ -1,13 +1,14 @@
+const date = new Date(Date.now());
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+	return knex('donations').then(function() {
+		return knex('donations').insert([
+			{ id: 1, donated_by: 6, donated_for: 3, donated_at: date.toDateString() },
+			{ id: 2, donated_by: 5, donated_for: 2, donated_at: date.toDateString() },
+			{ id: 3, donated_by: 4, donated_for: 1, donated_at: date.toDateString() },
+			{ id: 3, donated_by: 3, donated_for: 6, donated_at: date.toDateString() },
+			{ id: 3, donated_by: 2, donated_for: 5, donated_at: date.toDateString() },
+			{ id: 3, donated_by: 1, donated_for: 4, donated_at: date.toDateString() }
+		]);
+	});
 };
