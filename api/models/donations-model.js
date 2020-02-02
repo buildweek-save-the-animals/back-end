a2cd30f0c3d7727687ab46dedd9cd15f9e19209b
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-02-02 12:52:52 -0500

    get donations by campaign ID with total db function and endpoint authored

--- api/models/donations-model.js
+++ api/models/donations-model.js
@@ -1,9 +1,17 @@
 const db = require('../../data/dbConfig');
 
 const getDonosWithTotal = async donated_for => {
-	const donations = await db('donations').where({ donated_for });
+	const donations = await db
+		.select('donations.donation_amount as amount', 'users.username as donated_by', 'donations.donated_at')
+		.from('donations')
+		.join('users', 'users.id', 'donations.donated_by')
+		.where({ donated_for });
 
-	return donations;
+	const donoTotal = donations.reduce((acc, dono) => acc + dono.amount, 0);
+
+	return donations.length
+		? { total: donoTotal, ...donations }
+		: { total: 'No donations yet', donated_by: 'N/A', donated_at: 'N/A' };
 };
 
 module.exports = { getDonosWithTotal };
