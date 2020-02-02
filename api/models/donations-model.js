ffae55ade295850bb7d1553a887a4e1453b436d4
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-02-02 12:57:36 -0500

    normalized donations data

--- api/models/donations-model.js
+++ api/models/donations-model.js
@@ -10,8 +10,8 @@ const getDonosWithTotal = async donated_for => {
 	const donoTotal = donations.reduce((acc, dono) => acc + dono.amount, 0);
 
 	return donations.length
-		? { total: donoTotal, ...donations }
-		: { total: 'No donations yet', donated_by: 'N/A', donated_at: 'N/A' };
+		? { total: donoTotal, donations }
+		: { total: 'No donations yet', donations: [{ amount: 'N/A', donated_by: 'N/A', donated_at: 'N/A' }] };
 };
 
 module.exports = { getDonosWithTotal };
